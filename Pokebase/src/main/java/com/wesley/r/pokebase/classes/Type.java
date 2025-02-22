16a6e5deb701c82925c692001e888ca96485dbf5
WesleyR <wesleyroks4@gmail.com>
2025-02-22 12:16:32 +0100

    Adding type and zone

--- Pokebase/src/main/java/com/wesley/r/pokebase/classes/Type.java
+++ Pokebase/src/main/java/com/wesley/r/pokebase/classes/Type.java
@@ -1,4 +1,24 @@
 package com.wesley.r.pokebase.classes;
 
 public class Type {
+
+    private int typeId;
+    private String typeName;
+
+    public Type(int typeId, String typeName) {
+        this.typeId = typeId;
+        this.typeName = typeName;
+    }
+
+    public int getTypeId() {
+        return typeId;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
 }
--- Pokebase/src/main/java/com/wesley/r/pokebase/classes/Zone.java
+++ Pokebase/src/main/java/com/wesley/r/pokebase/classes/Zone.java
@@ -1,4 +1,24 @@
 package com.wesley.r.pokebase.classes;
 
 public class Zone {
+
+    private int zoneId;
+    private String zoneName;
+
+    public Zone(int zoneId, String zoneName) {
+        this.zoneId = zoneId;
+        this.zoneName = zoneName;
+    }
+
+    public int getZoneId() {
+        return zoneId;
+    }
+
+    public String getZoneName() {
+        return zoneName;
+    }
+
+    public void setZoneName(String zoneName) {
+        this.zoneName = zoneName;
+    }
 }
